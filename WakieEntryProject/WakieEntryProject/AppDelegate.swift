2941834f0dccf493fd483afc50c7006e8917bec7
Deniz Kaplan <kaplan.deni@sberbank.ru>
2021-07-03 18:07:58 +0300

    inital commit

---
+++ WakieEntryProject/WakieEntryProject/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  WakieEntryProject
+//
+//  Created by Deniz Kaplan on 03.07.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+		window = UIWindow(frame: UIScreen.main.bounds)
+		window?.rootViewController = ViewController()
+		window?.makeKeyAndVisible()
+
+		return true
+	}
+}
+
---
+++ WakieEntryProject/WakieEntryProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WakieEntryProject
+//
+//  Created by Deniz Kaplan on 03.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		view.backgroundColor = .red
+	}
+
+
+}
+
